70bd7f54c8a3d945cc6a84c04e0b6ff9647682b8
Marc Gueury <marc.gueury@oracle.com>
2026-01-30 20:28:25 +0100

    v3.0

---
+++ src/after_build.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+export SRC_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+export ROOT_DIR=${SRC_DIR%/*}
+cd $ROOT_DIR
+
+. ./starter.sh env
+
+title "INSTALLATION DONE"
+echo
+# echo "(experimental) Cohere with Tools and GenAI Agent:"
+# echo "http://${BASTION_IP}:8081/"
+# echo "-----------------------------------------------------------------------"
+
+echo "URLs" > $FILE_DONE
+append_done "-----------------------------------------------------------------------"
+append_done "OpenAI Gateway URLs:"
+append_done "- http://${BASTION_IP}/app/v1"
+append_done "- https://${APIGW_HOSTNAME}/${TF_VAR_prefix}/app/v1"
+append_done
+append_done "APIKEY: $TF_VAR_default_api_keys"
+append_done "MODEL: dac (see models.yaml)"
+append_done
+append_done "Endpoint OCID: $TF_VAR_dac_endpoint_ocid"
+append_done 
